a5e86bb63f21af3050a9e6b58080781e246794d0
keneleven <nattawut.eleven@mail.kmutt.ac.th>
2018-11-20 17:09:34 +0700

    add do-while loop Program#1

--- Lottery.c
+++ Lottery.c
@@ -4,10 +4,14 @@ void main()
   int WinLotteryNo[10] = {1522, 1711, 5515, 7233, 5614, 8876, 1215, 4232, 9155, 9995}; 
   float WinAmount[10] = {50000, 10000, 10000, 5000, 5000, 5000, 5000, 3000, 3000, 3000};
   int N = 10;
-  int i, won; 
+  int i, won;
   
-  int YourLotteryNo;  
-   
+  int YourLotteryNo;
+    i=0;
+    do{
+        i++;
+        printf("Lottery Program");
+    }while(i<5);
   printf("Enter your Lottery Number (0 to exit): "); 
   scanf("%d", &YourLotteryNo); 
   while (YourLotteryNo != 0) 
